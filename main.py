3ff3868cf8ad06bd8040be490c6d14276203407a
dauvet <vanhieu85@gmail.com>
2023-12-18 09:10:23 +0700

    Add new log

---
+++ main.py
@@ -0,0 +1,44 @@
+import logging
+import logging.handlers
+import os
+
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "quotes.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+    #logger.info("Token not available!")
+    #raise
+
+
+if __name__ == "__main__":
+    logger.info(f"Token value: {SOME_SECRET}")
+
+    url = "https://quotes15.p.rapidapi.com/quotes/random/"
+
+    headers = {
+        "X-RapidAPI-Key": "8ec19efd40msh793aba0a2348de8p1413b6jsnbe5c970c3771",
+        "X-RapidAPI-Host": "quotes15.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    print("data",data)
+    if data['content']:
+        logger.info(data['content'])
+    else:
+        logger.info(f'No data found')
+        
